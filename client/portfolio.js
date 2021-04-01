4c4049cdee3fd289a7927cfcc3c380e2d6fe8ce9
yan-podolak <yan78490@gmail.com>
2021-04-01 19:19:16 +0200

    update fetchproducts

--- client/portfolio.js
+++ client/portfolio.js
@@ -43,7 +43,7 @@ const setCurrentProducts = ({result, meta}) => {
 const fetchProducts = async (page = 1, size = 12) => {
   try {
     const response = await fetch(
-      `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`
+      `https://clear-fashion-ten.vercel.app/?page=${page}&size=${size}`
     );
     const body = await response.json();
 
